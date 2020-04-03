53d1783af36e389eb0911705b10c38ebd75aecc2
Prafull Sakpal <prafullsakpal158@gmail.com>
2020-04-03 08:59:28 +0530

    Switch between signup and login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-login',
@@ -11,9 +11,13 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
+  isLoginMode: boolean = true;
   credentialsForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
+  constructor(
+    private formBuilder: FormBuilder, private authService: AuthService, private router: Router,
+    private toastController: ToastController
+  ) { }
 
   ngOnInit() {
     this.credentialsForm = this.formBuilder.group({
@@ -23,17 +27,32 @@ export class LoginPage implements OnInit {
   }
 
   onSubmit() {
-    this.authService.login(this.credentialsForm.value).subscribe(() => {
-      this.router.navigate(['home'], { replaceUrl: true });
-    });
-  }
-
-  register() {
-    this.authService.register(this.credentialsForm.value).subscribe(() => {
+    if (!this.credentialsForm.valid) {
+      return;
+    }
+    if (this.isLoginMode) {
       this.authService.login(this.credentialsForm.value).subscribe(() => {
         this.router.navigate(['home'], { replaceUrl: true });
       });
+    }
+    else {
+      this.authService.register(this.credentialsForm.value).subscribe(response => {
+        console.log('Registered '+ response);
+        this.presentToast(response.toString());
+      });
+    }
+  }
+
+  switchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  async presentToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
     });
+    toast.present();
   }
 
 }
